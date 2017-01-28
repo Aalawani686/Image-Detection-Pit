56265d04b003ed64b24b1ed827bc5811e56d23d6
Andy Alawani <anialawani@gmail.com>
2017-01-27 20:23:55 -0800

    Sahith saved me

---
+++ Build/CMakeFiles/Image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Image.dir/main.cpp.o"
+  "Image.pdb"
+  "Image"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace cv;
 
 int threshold(Mat image){
 
+
      Mat img_hsv;
+     imshow("image", image);
      cvtColor(image, img_hsv, CV_BGR2HSV);
      std::vector<cv::Mat> channels;
      split(img_hsv, channels);
@@ -13,62 +15,52 @@ int threshold(Mat image){
      Mat threshLower, threshUpper;
      Mat result;
 
+
      threshold(hueOrig, threshLower, 60, 255, CV_THRESH_BINARY);
      threshold(hueOrig, threshUpper, 180, 255, CV_THRESH_BINARY_INV);
 
      result = threshLower & threshUpper;
 
+     imshow("threshold", result);
      Mat edges;
      Canny(result, edges, 100, 200);
      //does this work
-     int Width = image.cols * 2;
-     int Height = image.rows * 2;
-     Mat dst = Mat(Height, Width, CV_8UC3, Scalar(0,0,0));
-     Rect roi(Rect(0,0,Width * 2, Height * 2));
-     Mat targetROI = dst(roi);
-
+     imshow("Canny", edges);
      std::vector<std::vector<Point> > contours;
 
 
      cv::findContours(edges, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-     std::vector<std::Point> output;
+     std::vector<Point> output;
+     Mat drawing = Mat::zeros(edges.size(), CV_8UC3);
 
-     namedWindow("allFour", 1);
      for(int i = 0; i< contours.size(); i++) {
-       approxPolyDP(contours[i], output, cv::arcLength(cv::Mat(contours.at(i)), true) * 0.02
-       , bool closed);
+       approxPolyDP(contours[i], output, cv::arcLength(cv::Mat(contours.at(i)), true) * 0.02, true);
        if(output.size()==4){
-         image.copyTo(targetROI);
-         targetROI = dst(cv::Rect(Width,0,image1.cols, image1.rows));
-         result.copyTo(targetROI);
-         targetROI = dst(cv::Rect(0,Height,image1.cols, image1.rows));
-         edges.copyTo(targetROI);
-         //drawContours(output, contours, i, Scalar(255,0,0));
-         //TODO find a way to make drawContours a mat and put into big Mat
-         imshow("allFour", dst);
+
+         drawContours(drawing, contours, i, Scalar(255,0,0));
+         imshow("contours", drawing);
          break;
        }
 
      }
 
-     waitKey(0);
-
-
 
 }
 
-int main(int, char**)
+int main()
 {
-    VideoCapture cap(0); // open the default camera
+
+    VideoCapture cap(2); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
+    Mat frame;
     //Mat edges;
     //namedWindow("edges",1);
-    for(;;)
+    while(cv::waitKey(30) != 27)
     {
-        Mat frame;
+
         cap >> frame;
         threshold(frame);// get a new frame from camera
         /*cvtColor(frame, edges, COLOR_BGR2GRAY);
@@ -77,8 +69,11 @@ int main(int, char**)
         imshow("edges", edges);
         threshold(frame);*/
 
-        if(waitKey(30) >= 0) break;
     }
+
+    waitKey(0);
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
+
+
 }
